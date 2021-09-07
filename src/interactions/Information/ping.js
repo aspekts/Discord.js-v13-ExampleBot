4153526aba92a518cb25fe6e3859e8144837f204
Lukass <85770350+Lukass-0069@users.noreply.github.com>
2021-09-07 11:10:48 -0400

    Fixed ping.js
    Fixed error when using command.

--- src/interactions/Information/ping.js
+++ src/interactions/Information/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'ping',
-    description: 'Check my ping!',
-    run: async (client, interaction) => {
-        interaction.followUp({
-            content: `My ping is \`${client.ws.ping}\`!`
-        });
-    },
-};
+    description: 'My ping.',
+    run: async (client, interaction, args) => {
+        await interaction.reply({
+            content: 'My ping is `' + client.ws.ping + '`!'
+        })
+    }
+}
