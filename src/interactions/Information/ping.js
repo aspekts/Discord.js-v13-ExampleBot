8394ce8872b01c87a829129d24143cb523e4a7a6
aspekts <marcus.kamuntu@gmail.com>
2021-09-21 17:03:59 +0200

    Fix Context menu issues in handler file + folders

--- src/interactions/Information/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'My ping.',
-    run: async (client, interaction, args) => {
-        await interaction.reply({
-            content: 'My ping is `' + client.ws.ping + '`!'
-        })
-    }
-}
